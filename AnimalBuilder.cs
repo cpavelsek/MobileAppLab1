9e715d7fdf14e5c8ebc02f8e4e92dd954f1a0310
cpavelsek <57453934+cpavelsek@users.noreply.github.com>
2021-03-23 14:02:12 -0500

    Add files via upload

--- AnimalBuilder.cs
+++ AnimalBuilder.cs
@@ -14,44 +14,48 @@ namespace ConsoleApp1
 
             do
             {
-                
 
                 Console.WriteLine("What type of animal would you like to create? (Cat or Dog)");
                 var userAnimal = Console.ReadLine();
-
+               
+                //while(string.IsNullOrEmpty(userAnimal) || string.)
             
-                if (userAnimal == "dog" || userAnimal == "Dog")
-                {
-
-                    Console.WriteLine("What is your dogs name? ");
-                    var dogName = Console.ReadLine();
-
-                    Console.WriteLine("Is your dog friendly? (true or false) ");
-                    var dogFriendly = Console.ReadLine();
-
-                    if (dogFriendly.Equals("true", StringComparison.OrdinalIgnoreCase))
+                    if (userAnimal == "dog" || userAnimal == "Dog")
                     {
-                        zoo.Add(new Dog(true, dogName));
+
+                        Console.WriteLine("What is your dogs name? ");
+                        var dogName = Console.ReadLine();
+
+                        Console.WriteLine("Is your dog friendly? (true or false) ");
+                        var dogFriendly = Console.ReadLine();
+
+                        if (dogFriendly.Equals("true", StringComparison.OrdinalIgnoreCase))
+                        {
+                            zoo.Add(new Dog(true, dogName));
+                        }
+                        else if (dogFriendly == "false")
+                        {
+                            zoo.Add(new Dog(false, dogName));
+                        }
+                    
                     }
-                    else if (dogFriendly == "false")
+
+                    else if (userAnimal == "cat" || userAnimal == "Cat")
                     {
-                        zoo.Add(new Dog(false, dogName));
+
+                        Console.WriteLine("What is your cats name? ");
+                        var catName = Console.ReadLine();
+
+                        Console.WriteLine("How many mice has your cat killed? ");
+                        var deadMice = Convert.ToInt32(Console.ReadLine());
+
+                        zoo.Add(new Cat(deadMice, catName));
+                    
                     }
-                    
-                }
-
-                if (userAnimal == "cat" || userAnimal == "Cat")
-                {
-
-                    Console.WriteLine("What is your cats name? ");
-                    var catName = Console.ReadLine();
-
-                    Console.WriteLine("How many mice has your cat killed? ");
-                    var deadMice = Convert.ToInt32(Console.ReadLine());
-
-                    zoo.Add(new Cat(deadMice, catName));
-                    
-                }
+                    else 
+                    {
+                        Console.WriteLine("That wasn't an option. Try again");
+                    }
 
                 Console.WriteLine("Would you like to create another animal?  ('Q' to quit)");
                 input = Console.ReadLine();
